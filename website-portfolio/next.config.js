9478cd23517b8cadb8b1d55a2e072f0d659784bb
jonathanm35796 <jonathanm35796@users.noreply.github.com>
2025-02-04 07:01:36 -0700

    Update .gitignore, next.config.js, and add amplify.yml for environment configuration and build settings
    Signed-off-by: jonathanm35796 <jonathanm35796@users.noreply.github.com>

--- website-portfolio/next.config.js
+++ website-portfolio/next.config.js
@@ -2,8 +2,27 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'www.jonathansmcfarland.com'],
+    unoptimized: true
   },
+  output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
