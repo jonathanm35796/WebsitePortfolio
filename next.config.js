24fea22fb376b376fd28abb68b837686fac76f9f
jonathanm35796 <jonathanm35796@users.noreply.github.com>
2025-02-04 09:15:26 -0700

    Refactor Next.js configuration and update image paths; remove unused SVG assets and optimize image handling in Hero and Projects components
    Signed-off-by: jonathanm35796 <jonathanm35796@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -1,35 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
   images: {
-    domains: ['localhost', 'www.jonathansmcfarland.com', 'main.d2gzku5bh174dr.amplifyapp.com'],
     unoptimized: true
-  },
-  output: 'standalone',
-  poweredByHeader: false,
-  generateEtags: true,
-  compress: true,
-  productionBrowserSourceMaps: false,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on'
-          },
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains'
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-          }
-        ]
-      }
-    ]
   }
 }
 
